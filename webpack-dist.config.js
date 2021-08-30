3fdd9aab35c5e0043eb1225545ff178683705262
Mogo <cy1mxc@outlook.com>
2021-08-30 21:54:52 +0800

    update

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
-const path = require('path');
-const { spawnSync } = require('child_process');
-const findChrome = require('chrome-finder');
-const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
-const DefinePlugin = require('webpack/lib/DefinePlugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const EndWebpackPlugin = require('end-webpack-plugin');
-const { WebPlugin } = require('web-webpack-plugin');
-const ghpages = require('gh-pages');
+const fs = require("fs");
+const path = require("path");
+const { spawnSync } = require("child_process");
+const findChrome = require("chrome-finder");
+const UglifyJsPlugin = require("webpack/lib/optimize/UglifyJsPlugin");
+const DefinePlugin = require("webpack/lib/DefinePlugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const EndWebpackPlugin = require("end-webpack-plugin");
+const { WebPlugin } = require("web-webpack-plugin");
+const ghpages = require("gh-pages");
 
 function publishGhPages() {
   return new Promise((resolve, reject) => {
@@ -17,22 +17,22 @@ function publishGhPages() {
       } else {
         resolve();
       }
-    })
+    });
   });
 }
 
-const outputPath = path.resolve(__dirname, '.public');
+const outputPath = path.resolve(__dirname, ".public");
 module.exports = {
   output: {
     path: outputPath,
-    publicPath: '',
-    filename: '[name]_[chunkhash:8].js',
+    publicPath: "",
+    filename: "[name]_[chunkhash:8].js",
   },
   resolve: {
     // 加快搜索速度
-    modules: [path.resolve(__dirname, 'node_modules')],
+    modules: [path.resolve(__dirname, "node_modules")],
     // es tree-shaking
-    mainFields: ['jsnext:main', 'browser', 'main'],
+    mainFields: ["jsnext:main", "browser", "main"],
   },
   module: {
     rules: [
@@ -40,35 +40,35 @@ module.exports = {
         test: /\.scss$/,
         // 提取出css
         loaders: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
+          fallback: "style-loader",
           // 压缩css
-          use: ['css-loader?minimize', 'postcss-loader', 'sass-loader']
+          use: ["css-loader?minimize", "postcss-loader", "sass-loader"],
         }),
-        include: path.resolve(__dirname, 'src')
+        include: path.resolve(__dirname, "src"),
       },
       {
         test: /\.css$/,
         // 提取出css
         loaders: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
+          fallback: "style-loader",
           // 压缩css
-          use: ['css-loader?minimize', 'postcss-loader'],
+          use: ["css-loader?minimize", "postcss-loader"],
         }),
       },
       {
         test: /\.(gif|png|jpe?g|eot|woff|ttf|svg|pdf)$/,
-        loader: 'base64-inline-loader',
+        loader: "base64-inline-loader",
       },
-    ]
+    ],
   },
   entry: {
-    main: './src/main.js',
+    main: "./src/main.js",
   },
   plugins: [
     new DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+      },
     }),
     new UglifyJsPlugin({
       // 最紧凑的输出
@@ -84,30 +84,36 @@ module.exports = {
         collapse_vars: true,
         // 提取出出现多次但是没有定义成变量去引用的静态值
         reduce_vars: true,
-      }
+      },
     }),
     new WebPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
+      template: "./src/index.html",
+      filename: "index.html",
     }),
     new ExtractTextPlugin({
-      filename: '[name]_[contenthash:8].css',
+      filename: "[name]_[contenthash:8].css",
       allChunks: true,
     }),
     new EndWebpackPlugin(async () => {
       // 自定义域名
-      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'https://messiahhh.github.io/resume');
+      fs.writeFileSync(
+        path.resolve(outputPath, "CNAME"),
+        "https://mogomec.github.io/resume/"
+      );
 
       await publishGhPages();
 
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
-      spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'https://messiahhh.github.io/resume/' // 这里注意改成你的在线简历的网站
+      spawnSync(chromePath, [
+        "--headless",
+        "--disable-gpu",
+        `--print-to-pdf=${path.resolve(outputPath, "resume.pdf")}`,
+        "https://mogomec.github.io/resume/", // 这里注意改成你的在线简历的网站
       ]);
 
       // 重新发布到 ghpages
       await publishGhPages();
     }),
-  ]
+  ],
 };
